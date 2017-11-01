46d72aa2418c1c61516cef5ab0e4a00c1337e8bf
beautifulBoys <1298947916@qq.com>
2017-11-01 09:51:30 +0800

    client code

---
+++ client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+};
+</script>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ client/src/vuex/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+});
