c41a6a8fe47f110ada178b91006084e858d789b8
beautifulBoys <1298947916@qq.com>
2017-11-26 13:30:13 +0800

    lient router

--- server/src/router/client.js
+++ server/src/router/client.js
@@ -2,7 +2,6 @@
 import express from 'express';
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
--- server/src/router/index.js
+++ server/src/router/index.js
@@ -1,5 +1,7 @@
 import admin from './admin.js';
+import client from './client.js';
 
 export default (app) => {
   app.use('/admin', admin);
+  app.use('/client', client);
 };
