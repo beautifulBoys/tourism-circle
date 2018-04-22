d5a44f12c89e9cc430ca1a9267e913942267f20b
beautifulBoys <1298947916@qq.com>
2018-04-22 21:36:41 +0800

    修改数据表（document）初始化

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,18 +1,17 @@
 
 import mongoose from 'mongoose'
-import init_db from '../servers/init_db.js'
+import init_db_document from '../servers/init_db.js'
 
 function dbconnect () {
-
-  // mongoose.connect('mongodb://47.95.212.47:27017/tourism', {useMongoClient: true})
-  mongoose.connect('mongodb://127.0.0.1:27017/tourism', {useMongoClient: true})
+  mongoose.connect('mongodb://47.95.212.47:27017/tourism', {useMongoClient: true})
+  // mongoose.connect('mongodb://127.0.0.1:27017/tourism', {useMongoClient: true})
   var db = mongoose.connection
   db.on('error', () => {
     console.log('数据库连接出错。')
   })
   db.once('open', () => {
     console.log('数据库连接成功。')
-    init_db()
+    init_db_document()
   })
   db.on('close', () => {
     console.log('数据库已断开，请重新连接数据库')
--- server/src/servers/init_db.js
+++ server/src/servers/init_db.js
@@ -1,23 +1,23 @@
 import Id from '../model/id.js'
 
-export default () => {
+export default async () => {
   // 初始化时 创建id表
-  let obj = await Id.findOne({type: 'userId'})
-  if (obj) await Id.update({_id: obj._id}, {value: obj.value + 1}, {multi: false}, () => {})
+  let obj_user = await Id.findOne({type: 'userId'})
+  if (obj_user) await Id.update({_id: obj_user._id}, {value: obj_user.value + 1}, {multi: false}, () => {})
   else await Id.create({type: 'userId'})
 
   // 初始化 创建post表
-  let obj = await Id.findOne({type: 'postId'})
-  if (obj) await Id.update({_id: obj._id}, {value: obj.value + 1}, {multi: false}, () => {})
+  let obj_post = await Id.findOne({type: 'postId'})
+  if (obj_post) await Id.update({_id: obj_post._id}, {value: obj_post.value + 1}, {multi: false}, () => {})
   else await Id.create({type: 'postId'})
 
   // 初始化 创建message表
-  let obj = await Id.findOne({type: 'messageId'})
-  if (obj) Id.update({_id: obj._id}, {value: obj.value + 1}, {multi: false}, () => {})
+  let obj_message = await Id.findOne({type: 'messageId'})
+  if (obj_message) Id.update({_id: obj_message._id}, {value: obj_message.value + 1}, {multi: false}, () => {})
   else Id.create({type: 'messageId'})
 
   // 初始化 创建image表
-  let obj = await Id.findOne({type: 'imagesId'})
-  if (obj) await Id.update({_id: obj._id}, {value: obj.value + 1}, {multi: false}, () => {})
+  let obj_image = await Id.findOne({type: 'imagesId'})
+  if (obj_image) await Id.update({_id: obj_image._id}, {value: obj_image.value + 1}, {multi: false}, () => {})
   else await Id.create({type: 'imagesId'})
 }
